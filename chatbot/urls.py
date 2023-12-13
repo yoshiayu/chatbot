896f93d0be51a80c11085c092063ad05befee414
yoshiayu <yoshiayu19771106@gmail.com>
2023-12-13 15:29:08 +0900

    2ndcommit

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -5,3 +5,13 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('bot_response/', views.bot_response, name='bot_response'),
 ]
+
+# from django.contrib import admin
+# from django.urls import path, include
+
+# app_name = 'chatbot'
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', include('chatbot.urls')), # アクセス先のアプリを設定
+# ]
--- chatbotapp/urls.py
+++ chatbotapp/urls.py
@@ -1,8 +1,18 @@
-from django.urls import path
 from django.contrib import admin
-from chatbotapp import views
+from django.urls import path, include
+
+app_name = 'chatbot'
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('bot_response/', views.bot_response, name='bot_response'),
+    path('admin/', admin.site.urls),
+    path('', include('chatbot.urls')), # アクセス先のアプリを設定
 ]
+
+# from django.urls import path
+# from chatbotapp import views
+
+# urlpatterns = [
+#     path('', views.home, name='home'),
+#     path('bot_response/', views.bot_response, name='bot_response'),
+# ]
+
